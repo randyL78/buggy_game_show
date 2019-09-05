3d0aba12c00ffddc9cef9bf01fa4f81460051f6c
Randy Layne <randydavidl78@gmail.com>
2019-09-05 15:24:44 -0400

    Created first buggy version

--- app.js
+++ app.js
@@ -51,76 +51,8 @@ const addPhraseToDisplay = arr => {
 }
 
 
-
-// check if a letter is in the phrase
-const checkLetter = button => {
-
-    // get the LIs from the phrase container
-    let lis = phraseContainer.getElementsByTagName("LI");
-
-    // store whether a match is found in the phrase
-    let match = null;
-
-    // iterate through the phrase 
-    for (let i = 0; i < lis.length; i++) {
-
-        // test if the button's text matches the phrase's current character
-        if (lis[i].textContent.toLowerCase() === button.textContent.toLowerCase()) {
-            lis[i].classList.add("show");
-            match = button.textContent.toLowerCase();
-        }
-    }
-
-    return match;
-}
-
-// check if the game has been won or lost
-const checkWin = () => {
-
-    let letters = phraseContainer.getElementsByClassName("letter");
-    let shown = phraseContainer.getElementsByClassName("show");
-
-    // if the length of the arrays are the same, the user won
-    if (letters.length === shown.length) {
-
-        // change start screen to win screen
-        overlay.classList.add('win');
-        overlay.querySelector("h2").textContent = "You Win";
-
-        // show the win screen overlay
-        overlay.style.display = "flex";
-    }
-
-    if (missed > 4) {
-
-        // change start screen to lose screen
-        overlay.classList.add('lose');
-        overlay.querySelector("h2").textContent = "You Lose";
-
-        // show the lose screen overlay
-        overlay.style.display = "flex";
-    }
-}
-
-
 // listen for the reset game button to be pressed
 resetButton.addEventListener('click', () => {
-
-    // reset game
-    missed = 0;
-    phraseContainer.innerHTML = "";
-
-    for (let i = 0; i < 5; i++) {
-        tries[i]
-            .getElementsByTagName("IMG")[0]
-            .setAttribute("src", "images/liveHeart.png");
-    }
-
-    let keys = qwerty.getElementsByTagName('BUTTON');
-
-    for (let i = 0; i < keys.length; i++) {
-        keys[i].classList.remove("chosen");
-    }
 
     // select a random string from the array
     phrase = getRandomPhraseAsArray(phrases);
@@ -134,31 +66,4 @@ resetButton.addEventListener('click', () => {
     overlay.classList.remove("win", "lose");
 });
 
-
-// listen for the onscreen keyboard to be clicked
-qwerty.addEventListener('click', e => {
-
-    // filter out whitespace clicks and buttons that are already pressed
-    if (e.target.tagName === 'BUTTON' && !e.target.classList.contains("chosen")) {
-
-        // disable the button so that it can't be clicked twice
-        e.target.classList.add("chosen");
-
-        let match = checkLetter(e.target);
-
-        // increment missed counter if not a match
-        if (!match) {
-
-            // change the current heart to a lossed heart
-            let heartImage = tries[missed].getElementsByTagName("IMG")[0];
-            heartImage.setAttribute("src", "images/lostHeart.png");
-
-            // increment counter
-            missed++;
-        }
-
-        // chek for a winner or loser
-        checkWin();
-    }
-})
 
